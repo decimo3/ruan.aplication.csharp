3eab509967fb6764708fe0276ae1ad87a76bc8a7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-06-30 14:04:18 -0300

    INIT: blank dotnet project

---
+++ ruan.aplication.cs
@@ -0,0 +1,10 @@
+﻿using basedados;
+namespace aplication {
+  static class principal {
+    static void Main (string[] args){
+      System.Console.WriteLine("Olá mundo!");
+      var banco = new mariadb();
+      banco.conectar();
+    }
+  }
+}
