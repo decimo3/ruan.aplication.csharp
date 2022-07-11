18c70df3ac6180b58a961b98c69e84ea71ca3828
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-07-11 18:03:22 -0300

    FIX: regular expression correction

--- Verificador.cs
+++ Verificador.cs
@@ -3,13 +3,13 @@ namespace Verificador
 {
     public static class verificarse
     {
-        public static bool validoUsuario(string valor)
+        public static bool validoUsuario(string texto)
         {
-            return (Regex.IsMatch(texto, "[0-9A-z]{5,16}")) ? true : false;
+            return (Regex.IsMatch(texto, "^[A-Z][a-z0-9]{4,16}$")) ? true : false;
         }
-        public static bool validoPalavra(string valor)
+        public static bool validoPalavra(string texto)
         {
-            return (Regex.IsMatch(texto, "[0-9A-z]{8,32}")) ? true : false;
+            return (Regex.IsMatch(texto, "^[A-Z][a-z0-9]{7,32}$")) ? true : false;
         }
     }
 }
